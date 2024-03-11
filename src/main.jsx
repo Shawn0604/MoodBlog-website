567b162c0030090359346e09208d9c50b7624118
Shawn0604 <shawn901011@gmail.com>
2024-03-11 17:21:52 +0800

    0311-前端修改
    0311-前端修改

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import ThemeProvider from "./utils/ThemeContext";
 import App from "./App";
 
-// basic setting. you can ignore it.
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router >
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
